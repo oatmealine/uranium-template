927ad2bdb288a01f94e212df38061c1312c1d0e2
Jade "oatmealine" Monoids <oatmealine@disroot.org>
2026-06-18 17:15:50 +0300

    refactor aft module

--- template/stdlib/aft.lua
+++ template/stdlib/aft.lua
@@ -1,36 +1,59 @@
--- TODO: make this not suck (use contexts pleaseeee)
-
-local uranium = require 'uranium'
-local events = require 'uranium.events'
-
 local self = {}
 
-function self.sprite(self)
-  self:basezoomx(sw / dw)
-  self:basezoomy(-sh / dh)
-  self:x(scx)
-  self:y(scy)
+-- Sets up a Sprite for textures from AFTs, scaling it with the display
+-- resolution
+---@param sprite Sprite
+function self.setupSprite(sprite)
+  sprite:basezoomx(sw / dw)
+  sprite:basezoomy(-sh / dh)
+  sprite:x(scx)
+  sprite:y(scy)
 end
 
-function self.aft(self)
-  self:SetWidth(dw)
-  self:SetHeight(dh)
-  self:EnableDepthBuffer(false)
-  self:EnableAlphaBuffer(false)
-  self:EnableFloat(false)
-  self:EnablePreserveTexture(true)
-  self:Create()
+-- Creates a Sprite for textures from AFTs, scaling it with the display
+-- resolution
+--
+-- Like `setupSprite`, but includes creating the Sprite from a Context
+---@param ctx Context
+function self.createSprite(ctx)
+  local sprite = ctx:Sprite()
+  self.setupSprite(sprite)
+  return sprite
 end
 
-function self.aftSetup()
-	local a = uranium.ctx:ActorFrameTexture()
-	local b = uranium.ctx:Sprite()
-  events:on('init', function()
-    self.aft(a)
-		self.sprite(b)
-		b:SetTexture(a:GetTexture())
+-- Sets up an AFT that will capture the entire scene when drawn
+---@param aft ActorFrameTexture
+function self.setupAFT(aft)
+  aft:SetWidth(dw)
+  aft:SetHeight(dh)
+  aft:EnableDepthBuffer(false)
+  aft:EnableAlphaBuffer(false)
+  aft:EnableFloat(false)
+  aft:EnablePreserveTexture(true)
+  aft:Create()
+end
+
+-- Creates an AFT that will capture the entire scene when drawn
+--
+-- Like `setupAFT`, but includes creating the AFT from a Context
+---@param ctx Context
+function self.createAFT(ctx)
+  local aft = ctx:ActorFrameTexture()
+  self.setupAFT(aft)
+  return aft
+end
+
+-- Creates a sprite with `createSprite`, an AFT with `createAFT`, and assigns
+-- the sprite a texture from the AFT, returning both
+---@param ctx Context
+---@return ActorFrameTexture, Sprite
+function self.createEffectStack(ctx)
+	local aft = self.createAFT(ctx)
+  local sprite = self.createSprite(ctx)
+  sprite:addcommand('Init', function()
+		sprite:SetTexture(aft:GetTexture())
   end)
-	return a, b
+	return aft, sprite
 end
 
 return self
