3eeef3f97548d782e4c7ff730207079723f0ba73
Jade "oatmealine" Monoids <oatmealine@disroot.org>
2026-06-18 17:18:17 +0300

    refactor env to not use util

--- template/stdlib/env.lua
+++ template/stdlib/env.lua
@@ -1,8 +1,12 @@
-require('stdlib.util')
-
 local self = {}
 
 self.inEditor = SCREENMAN:GetTopScreen():GetName() == 'ScreenEdit'
-self.onWine = includes(INPUTMAN:GetDescriptions(), 'Wine Keyboard')
+self.onWine = false
+for _, keyboard in ipairs(INPUTMAN:GetDescriptions()) do
+  if keyboard == 'Wine Keyboard' then
+    self.onWine = true
+    break
+  end
+end
 
 return self
